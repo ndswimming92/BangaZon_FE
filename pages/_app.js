7afa93e5253e5baf4c324236f80ceeecd1cbbe00
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-03-02 13:16:11 -0600

    updated pages file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import NoAuth from '../components/NoAuth'; // TODO: COMMENT OUT FOR AUTH
 import '../styles/globals.css';
 import { AuthProvider } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector'; // TODO: COMMENT IN FOR AUTH
